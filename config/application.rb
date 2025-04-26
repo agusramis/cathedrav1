3ff15a2c5b30bffae2f2a64ba77b2bd3e8af7bd8
unknown <ramisagustin@gmail.com>
2025-04-26 19:04:30 -0300

    Fix assets precompile to avoid Sprockets 4 error

--- config/application.rb
+++ config/application.rb
@@ -35,48 +35,47 @@ module Cathedra
 
     # setup npm folder for lookup
     config.assets.paths << Rails.root.join('vendor', 'assets', 'node_modules')
-    # fonts
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-    # images
-    config.assets.precompile << /\.(?:png|jpg)$/
-    # precompile vendor assets
-    config.assets.precompile += %w( base.js )
-    config.assets.precompile += %w( base.css )
-    # precompile themes
-    config.assets.precompile += ['site/themes/theme-a.css']
-    # Controller assets
-    config.assets.precompile += [
-      # Scripts
-      'charts.js',
-      'dashboard.js',
-      'elements.js',
-      'extras.js',
-      'forms.js',
-      'maps.js',
-      'multilevel.js',
-      'pages.js',
-      'tables.js',
-      'widgets.js',
-      'blog.js',
-      'ecommerce.js',
-      'forum.js',
-      # Stylesheets
-      'charts.css',
-      'dashboard.css',
-      'elements.css',
-      'extras.css',
-      'forms.css',
-      'maps.css',
-      'multilevel.css',
-      'pages.css',
-      'tables.css',
-      'widgets.css',
-      'blog.css',
-      'ecommerce.css',
-      'forum.css'
-    ]
+# fonts
+    config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
+# images
+    config.assets.precompile += %w( *.png *.jpg )
+# precompile vendor assets
+    config.assets.precompile += %w( base.js base.css )
+# precompile themes
+config.assets.precompile += ['site/themes/theme-a.css']
+# Controller assets
+config.assets.precompile += [
+  # Scripts
+  'charts.js',
+  'dashboard.js',
+  'elements.js',
+  'extras.js',
+  'forms.js',
+  'maps.js',
+  'multilevel.js',
+  'pages.js',
+  'tables.js',
+  'widgets.js',
+  'blog.js',
+  'ecommerce.js',
+  'forum.js',
+  # Stylesheets
+  'charts.css',
+  'dashboard.css',
+  'elements.css',
+  'extras.css',
+  'forms.css',
+  'maps.css',
+  'multilevel.css',
+  'pages.css',
+  'tables.css',
+  'widgets.css',
+  'blog.css',
+  'ecommerce.css',
+  'forum.css'
+]
 
-    # Default language
+# Default language
     config.i18n.default_locale = :'es'
     # config.action_view.field_error_proc = Proc.new { |html_tag, instance| "#{html_tag}".html_safe }
     # Mostrar errores inline sin agregar div porque modifica la estructura, utiliza span
@@ -90,5 +89,5 @@ module Cathedra
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-  end
+end
 end
