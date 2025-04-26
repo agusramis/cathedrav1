4c70b7b644e669f017b1d99612057d6a7340ca1a
unknown <ramisagustin@gmail.com>
2025-04-26 19:24:14 -0300

    Clean missing requires from base.js

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,12 +1,2 @@
-// This is a manifest file that'll be compiled into base.js, which will include all the files
-// listed below.
-
-//--- Modernizr
-//--- Storage API
-//--- Screenfull
-//= require screenfull/dist/screenfull
-//--- jQuery
-//= require jquery/dist/jquery
-//--- Bootstrap
-//= require popper.js/dist/umd/popper
-//= require bootstrap/dist/js/bootstrap
+//= require jquery
+//= require bootstrap-sprockets
